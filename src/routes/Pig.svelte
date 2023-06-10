63f8a5289ca782217426f8c44b90e654d18c719b
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2023-06-10 04:40:46 -0700

    Add pig favicon and refactor Pig

--- src/routes/Pig.svelte
+++ src/routes/Pig.svelte
@@ -1,22 +1,25 @@
 <script>
-	import position from './positions';
+	import mousepos from './positions';
 	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store';
 	import pig from '$lib/images/pig.svg';
 	import righteye from '$lib/images/right-eye.svg';
 	import lefteye from '$lib/images/left-eye.svg';
 	import { browser } from '$app/environment';
 
-	const pigpos = writable({ x: 0, y: 0 });
+	let pigpos = {x:0, y:0};
 	onMount(() => {
-		// Function to update the position of the "Pig" div
+		// update the position of the "Pig" div
+		// Do on mount since only needs to happen once
 		function updatePosition() {
 			const pigDiv = document.getElementById('Pig');
 			if (pigDiv) {
+				// get dimensional data of pig div
 				const { left, top, width, height } = pigDiv.getBoundingClientRect();
+				// store center of div
 				let x = left + width / 2;
 				let y = top + height / 2;
-				pigpos.set({ x, y });
+				// update state variable
+				pigpos = { x, y };
 			}
 		}
 
@@ -34,20 +37,25 @@
 		};
 	});
 
-	$: dx = $position.x - $pigpos.x;
-	$: dy = $position.y - $pigpos.y;
+	// calculate delta between cursor and pig
+	$: dx = $mousepos.x - pigpos.x;
+	$: dy = $mousepos.y - pigpos.y;
     
-    const min = 0;
-    const max = 15;
+	// rerun updateCSS whenever mouse position changes
+	$: dx && dy && updateCSS();
 
+	// set max amount of eye movement in pixels
+    const max = 15;
 	function updateCSS() {
-		if (!browser) return;
+		if (!browser) return; // don't run on server
+		// get elements from DOM
 		let left = document.getElementById('lefteye');
 		let right = document.getElementById('righteye');
-		if (left && right) {
-            console.log(left, right)
+		if (left && right) { // wait for mount
+			// calculate desired eye translation
             let osx = (dx / screen.width) * max;
             let osy = (dy / screen.height) * max;
+			// set eye translation amount by setting css variables
 			left.style.setProperty('--offset-x', osx + 'px');
 			left.style.setProperty('--offset-y', osy + 'px');
 			right.style.setProperty('--offset-x', osx + 'px');
@@ -55,7 +63,6 @@
 		}
 	}
 
-	$: dx && dy && updateCSS();
 </script>
 
 <div id="container">
@@ -67,6 +74,9 @@
 </div>
 
 <style>
+	/* Wrap Pig in a container so pig can be absolute styled 
+	 * while overall pig element respects other elements
+	 */
     #container {
         position: relative;
         width: 100%;
@@ -74,10 +84,12 @@
         justify-content: center;
         align-items: center;
     }
+
+	/* position absolute so eye position can be hardcoded */
 	#Pig {
 		width: fit-content;
 		position: absolute;
-        top: 0;
+        top: 0; /* Move to start of container */
 	}
 
 	#lefteye,
@@ -87,6 +99,7 @@
 		transform: translate(var(--offset-x), var(--offset-y));
 	}
 
+	/* Set initial positioning to line up with eye socket */
 	#lefteye {
 		left: 7ch;
 		top: 7ch;
--- src/routes/positions.js
+++ src/routes/positions.js
@@ -1,6 +1,13 @@
 import { readable } from "svelte/store";
 import { browser } from "$app/environment";
 
+/* 
+ * Svelte store that contains the mouse position 
+ * data. Use for when an element needs global access
+ * to cursor position, otherwise just use easier
+ * on:mousemove binding.
+ * @returns {x:number y:number} coordinates of mouse relative to page. (0, 0) represents the top left corner.
+ */
 export default readable({x:0, y:0}, function start(set) {
     if (!browser) return;
 
