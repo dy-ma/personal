d6df4ed704ce346d7dc1beed3acb5c095402e50d
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2023-11-15 13:20:19 -0800

    Add custom 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+    import sleep from '$lib/images/sleeping.jpg'
+</script>
+
+<div class="error-page">
+	<div class="imgbox">
+		<img src={sleep} alt="Sleeping Hamster" />
+	</div>
+	<div class="error-message">
+		<div>
+			<h1>Oops! - 404</h1>
+			<p>He fell asleep.</p>
+		</div>
+	</div>
+</div>
+
+<style>
+    .error-page {
+        padding: 1em;
+        display: grid;
+        place-content: center;
+    }
+
+	img {
+		border-radius: 8px;
+	}
+</style>
